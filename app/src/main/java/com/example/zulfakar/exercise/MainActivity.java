67a4bf35f6a03d8bf9ab949064b5bf44c25319d0
zulfakarelzaf <muhammad.zulfakar@students.amikom.ac.id>
2018-07-20 07:54:09 +0700

    initialize the butterknife

--- app/src/main/java/com/example/zulfakar/exercise/MainActivity.java
+++ app/src/main/java/com/example/zulfakar/exercise/MainActivity.java
@@ -2,12 +2,23 @@ package com.example.zulfakar.exercise;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+    @BindView(R.id.iniTextView)
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @OnClick(R.id.iniTextView)
+    void clickTextView(){
+        textView.setText("hehe");
     }
 }
